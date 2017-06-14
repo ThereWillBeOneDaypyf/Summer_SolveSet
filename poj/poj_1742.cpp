6f72b45b23026d9e3ff972967a2a6598613b8049
ThereWillBeOneDaypyf <527908203@qq.com>
2017-06-14 14:24:58 +0800

    Update 2017/6/14

---
+++ poj/poj_1742.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<vector>
+#include<cmath>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<stack>
+#include<map>
+#include<set>
+using namespace std;
+
+//thanks to pyf ...
+//thanks to qhl ...
+
+#define INF 0x3f3f3f3f
+#define CLR(x,y) memset(x,y,sizeof(x))
+#define mp(x,y) make_pair(x,y)
+typedef pair<int, int> PII;
+typedef long long ll;
+
+const int N = 1e6 + 5;
+
+
+int v[N];
+int cnt[N];
+int dp[N];
+void compelte_bag(int pos, int m)
+{
+	for (int i = v[pos]; i <= m; i++)
+		if (dp[i - v[pos]] != -1)
+			dp[i] = dp[i - v[pos]] ;
+}
+void zero_one_bag(int pos, int m)
+{
+	int base = 1;
+	int ret = cnt[pos];
+	while (ret >= base)
+	{
+		ret -= base;
+		int wei = base * v[pos];
+		for (int i = m; i >= wei; i--)
+			if (dp[i - wei] != -1)
+				dp[i] = dp[i - wei] ;
+		base *= 2;
+	}
+	int wei = ret * v[pos];
+	if (wei)
+		for (int i = m; i >= wei; i--)
+			if (dp[i - wei] != -1)
+				dp[i] = dp[i - wei];
+}
+
+
+int main()
+{
+	int n, m;
+	while (cin >> n >> m)
+	{
+		if (!n && !m)
+			break;
+		for (int i = 0 ; i < n; i++)
+			cin >> v[i];
+		for (int i = 0; i < n; i++)
+			cin >> cnt[i];
+		CLR(dp, -1);
+		dp[0] = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (cnt[i]*v[i] >= m)
+				compelte_bag(i, m);
+			else
+				zero_one_bag(i, m);
+		}
+		int ans = 0;
+		for (int i = 1; i <= m; i++)
+			if (dp[i] != -1)
+				ans++;
+		cout << ans << endl;
+	}
+	return 0;
+}
