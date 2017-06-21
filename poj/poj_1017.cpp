6fbc554e0edb7aa222ccc03ce3df07e26aedeccd
ThereWillBeOneDaypyf <527908203@qq.com>
2017-06-22 03:47:47 +0800

    2017年 06月 22日 星期四 03:47:47 CST

--- poj/poj_1017.cpp
+++ poj/poj_1017.cpp
@@ -10,25 +10,25 @@ int a[10];
 
 bool judge()
 {
-	for(int i = 1;i<=6;i++)
-		if(a[i])
+	for (int i = 1;i <= 6;i++)
+		if (a[i])
 			return true;
 	return false;
 }
 
 int main()
 {
-	while(cin >> a[1] >> a[2] >> a[3] >> a[4] >> a[5] >> a[6] )
+	while (cin >> a[1] >> a[2] >> a[3] >> a[4] >> a[5] >> a[6] )
 	{
-		if(!judge())
+		if (!judge())
 			break;
 		long long ans = 0;
-		for(int i = 1; i<= 6;i++)
+		for (int i = 1; i<= 6;i++)
 		{
 			ans += i * i * a[i] ;
 		}
 		int tot = 0;
-		for(int i = 6; i>= 1;i--)
+		for (int i = 6; i>= 1;i--)
 		{
 			while(ans >= i*i)
 			{
