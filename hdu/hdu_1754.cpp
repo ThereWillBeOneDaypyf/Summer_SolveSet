10cf7ecbe590fe9892b2ba96d9596acdbb2bc9c5
ThereWillBeOneDaypyf <527908203@qq.com>
2017-06-25 18:14:03 +0800

    2017年 06月 25日 星期日 18:14:03 CST

--- hdu/hdu_1754.cpp
+++ hdu/hdu_1754.cpp
@@ -6,72 +6,132 @@ using namespace std;
 
 const int N = 200005;
 
-struct Tree
+int ch[N][2];
+int fa[N];
+int key[N];
+int Max[N];
+int v[N];
+int tot = 0;
+int root = 0;
+void NewNode(int &r,int k,int val,int Fa)
 {
-	int l,r;
-	int Max;
-	int step;
-}t[N*4];
-
-void push_up(int step)
-{
-	t[step].Max = max(t[step*2].Max ,t[step*2+1].Max);
+	r = ++ tot ;
+	ch[r][0] = ch[r][1] = 0;
+	key[r] = k;
+	v[r] = val;
+	Max[r] = val;
+	fa[r] = Fa;
 }
-void build(int l,int r,int step)
+void push_up(int r)
 {
-	t[step].l = l,t[step].r = r;
-	t[step].Max = 0;
-	if(l == r)
+	Max[r] = max(max(Max[ch[r][0]] , Max[ch[r][1]]),v[r]);
+}
+void init()
+{
+	tot = 0;
+	memset(ch,0,sizeof(ch));
+	memset(key,0,sizeof(key));
+	memset(Max,0,sizeof(Max));
+	memset(fa,0,sizeof(fa));
+	memset(v,0,sizeof(v));
+	int r;
+}
+void Rotate(int r,int kind) // 0 left 1 right
+{
+	int y = fa[r];
+	fa[ch[r][kind]] = y;
+	ch[y][!kind] = ch[r][kind];
+	if(fa[y])
+		ch[fa[y]][ch[fa[y]][1] == y] = r;
+	fa[r] = fa[y];
+	ch[r][kind] = y;
+	fa[y] = r;
+	push_up(y);
+	push_up(r);
+}
+void splay(int r,int goal)
+{
+	while(fa[r] != goal)
 	{
-		scanf("%d",&t[step].Max);
-		return ;
+		if(fa[fa[r]] == goal)
+			Rotate(r,ch[fa[r]][0] == r);
+		else
+		{
+			int x = fa[fa[r]];
+			int y = fa[r];
+			int kind_xy = ch[x][0] == y;
+			int kind_yr = ch[y][0] == r;
+			if(kind_xy == kind_yr)
+			{
+				Rotate(y,kind_xy);
+				Rotate(r,kind_yr);
+			}
+			else
+			{
+				Rotate(r,kind_yr);
+				Rotate(r,kind_xy);
+			}
+		}
 	}
-	int mid = (l + r)/2;
-	build(l,mid,step*2);
-	build(mid+ 1,r,step*2+1);
-	push_up(step);
+	if(!goal)
+		root = r;
+	push_up(r);	
 }
-void update(int x,int val,int step)
+void insert(int k,int val)
 {
-	if(t[step].l == t[step].r)
-	{
-		t[step].Max = val;
-		return;
-	}
-	int mid = (t[step].l + t[step].r) /2;
-	if(x <= mid)
-		update(x,val,step*2);
-	else
-		update(x,val,step*2+1);
-	push_up(step);
+	int r = root;
+	while(ch[r][k > key[r]])
+		r = ch[r][k > key[r]];
+	NewNode(ch[r][k > key[r]],k,val,r);
+	splay(ch[r][k > key[r]] , 0);
 }
-int query(int l,int r,int step)
+void update(int pos,int val)
 {
-	if(t[step].l == l && t[step].r == r)
-		return t[step].Max;
-	int mid = (t[step].l + t[step].r)/2;
-	if(r <= mid)
-		return query(l,r,step*2);
-	else if(l > mid)
-		return query(l,r,step*2+1);
-	else
-		return max(query(l,mid,step*2),query(mid+1,r,step*2+1));
+	v[pos] = val;
+	splay(pos,0);
+}
+int key_to_Index(int tar)
+{
+	int r = root;
+	while(key[r] != tar)
+		r = ch[r][tar > key[r]]; 
+	return r;
 }
 int main()
 {
-	int n,q;
-	while(scanf("%d%d",&n,&q)==2)
+	int n,m;
+	while(scanf("%d%d",&n,&m)==2)
 	{
-		build(1,n,1);
-		for(int i =1;i<=q;i++)
+		init();
+		NewNode(root,0,-1,0);
+		NewNode(ch[root][1],n+1,-1,root);
+		for(int i = 1;i<=n;i++)
+		{
+			int x;
+			scanf("%d",&x);
+			insert(i,x);
+		}
+		for(int i = 0;i<m;i++)
 		{
 			char op[5];
 			int x,y;
 			scanf("%s%d%d",op,&x,&y);
-			if(op[0] == 'Q')
-				printf("%d\n",query(x,y,1));
+			if(op[0]=='Q')
+			{
+				int l = key_to_Index(x-1);
+				int r = key_to_Index(y+1);
+				splay(l,0);
+				splay(r,l);	
+				printf("%d\n",Max[ch[r][0]]);
+			}
 			else
-				update(x,y,1);
+			{
+				int pos = key_to_Index(x);
+				update(pos,y);
+			}
+	//		for(int i = 3;i<=tot;i++)
+	//			cout << key[i] << " " << Max[i] << endl;
 		}
 	}
 }
+
