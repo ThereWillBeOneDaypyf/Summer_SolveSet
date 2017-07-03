a85e5491b3d9ed05f5d609e17157b3fb18a91590
ThereWillBeOneDaypyf <527908203@qq.com>
2017-07-03 17:35:38 +0800

    2017年 07月 03日 星期一 17:35:38 CST

--- codeforces/cf_822_c.cpp
+++ codeforces/cf_822_c.cpp
@@ -4,65 +4,51 @@ using namespace std;
 //thanks to pyf ...
 //thanks to qhl ...
 
-const int N = 200005;
+#define INF 0x3f3f3f3f
 
-struct Node
-{
-	int l,r,c;
-	bool operator < (const Node &rhs) const 
-	{
-		return c < rhs.c;
-	}
-};
-set<Node>s[N];
-map<int,int> len;
-int vis[N];
+const int N = 2e5 + 7;
+
+vector<pair<int,int> > l[N];
+vector<pair<int,int> > r[N];
+long long c[N];
+
 int main()
 {
-	int n,m;
-	while(cin >> n >> m)
+	int n,x;
+	while(cin >> n >> x)
 	{
-		for(int i = 0;i<N;i++)
-			s[i].clear();
-		len.clear();
 		for(int i = 0;i<n;i++)
 		{
-			int l,r,c;
-			cin >> l >> r >> c;
-			int tar = r - l + 1;
-			s[tar].insert({l,r,c});
-			len[tar] = 1;
+			int ll,rr,cc;
+			cin >> ll >> rr >> cc;
+			l[ll].push_back(make_pair(rr - ll + 1 ,cc));
+			r[rr].push_back(make_pair(rr - ll + 1 ,cc));	
 		}
-		map<int,int> :: iterator it;
-		int ans = -1;
-		for(it = len.begin();it != len.end();it++)
+		for(int i = 0;i< N;i++)
+			c[i] = INF;
+		long long ans = 2 * INF;
+		for(int i = 1;i<=200000;i++)
 		{
-			int tar = it->first;
-			if(vis[tar])
-				continue;
-			int next = m - tar;
-			vis[tar] = 1;
-			if(!len.count(next))
-				continue;
-			vis[next] = 1;
-			set<Node> :: iterator it1;
-			set<Node> :: iterator it2;
-			for(it1 = s[tar].begin() ; it1 != s[tar].end();it1++)
+			for(int j = 0;j<l[i].size();j++)
 			{
-				for(it2 = s[next].begin() ; it2 != s[next].end();it2++)
-				{
-					Node temp1 = *it1;
-					Node temp2 = *it2;
-					if(temp1.r < temp2.l || temp2.r < temp1.l)
-					{
-						if(ans == -1)
-							ans = temp1.c + temp2.c;
-						else
-							ans = min(ans,temp1.c + temp2.c);
-					}
-				}
+				int len = l[i][j].first;
+				int cost = l[i][j].second;
+				if(len >= x)
+					continue;
+				if(c[x - len] != INF)
+					ans = min(ans,c[x-len] + cost);
 			}
-		}	
-		cout << ans << endl;
-	}
+			for(int j = 0;j<r[i].size();j++)
+			{
+				long long len = r[i][j].first;
+				long long cost = r[i][j].second;
+				c[len] = min(c[len] ,cost);
+			}
+		}
+		if(ans == 2*INF)
+			cout << -1 << endl;
+		else
+			cout << ans << endl;
+	}	
+	return 0;
 }
