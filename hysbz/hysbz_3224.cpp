d708a2218d9b61fdd0cd319070b92f2a7b2b78f4
ThereWillBeOneDaypyf <527908203@qq.com>
2017-07-14 15:44:30 +0800

    2017年07月14日 15:44:29

--- hysbz/hysbz_3224.cpp
+++ hysbz/hysbz_3224.cpp
@@ -7,14 +7,14 @@ using namespace std;
 //thanks to pyf ...
 //thanks to qhl ...
 
-#define INF 2e9+7 
+#define INF 2e9+7
 
 const int N = 500015;
 
-int ch[N][2],fa[N],key[N],sz[N];
+int ch[N][2], fa[N], key[N], sz[N];
 int cnt[N];
-int n,q;
-int tot = 0,root = 0;
+int n, q;
+int tot = 0, root = 0;
 
 int rev[N];
 void clean(int r)
@@ -23,7 +23,7 @@ void clean(int r)
 }
 void mid_order(int r)
 {
-	if(!r)
+	if (!r)
 		return;
 	mid_order(ch[r][0]);
 	cout << key[r] << " ";
@@ -32,12 +32,12 @@ void mid_order(int r)
 
 void push_up(int r)
 {
-	if(!r)
+	if (!r)
 		return;
 	sz[r] =  cnt[r];
-	if(ch[r][0])
+	if (ch[r][0])
 		sz[r] += sz[ch[r][0]];
-	if(ch[r][1])
+	if (ch[r][1])
 		sz[r] += sz[ch[r][1]];
 }
 void push_down(int r)
@@ -85,71 +85,71 @@ void splay(int x, int goal)
 	push_up(x);
 	if (goal == 0) root = x;
 }
-void NewNode(int &r,int k,int Fa)
+void NewNode(int &r, int k, int Fa)
 {
 	r = ++ tot;
 	ch[r][0] = ch[r][1] = 0;
 	sz[r] = 1;
 	key[r] = k;
 	cnt[r] = 1;
-	fa[r] = Fa,rev[r] = 0;
+	fa[r] = Fa, rev[r] = 0;
 }
-void insert(int r,int val)
+void insert(int r, int val)
 {
-	if(!root)
+	if (!root)
 	{
-		NewNode(root,val,0);
+		NewNode(root, val, 0);
 		return;
 	}
-	if(key[r] == val)
+	if (key[r] == val)
 	{
 		cnt[r] ++;
-		splay(r,0);
+		splay(r, 0);
 		return;
 	}
-	if(!ch[r][val > key[r]])
+	if (!ch[r][val > key[r]])
 	{
-		NewNode(ch[r][val>key[r]],val,r);
-		splay(ch[r][val > key[r]],0);
+		NewNode(ch[r][val > key[r]], val, r);
+		splay(ch[r][val > key[r]], 0);
 		return;
 	}
-	else insert(ch[r][val>key[r]],val);
+	else insert(ch[r][val > key[r]], val);
 }
 void init()
 {
 	tot = root = 0;
-	memset(sz,0,sizeof(sz));
-	memset(fa,0,sizeof(fa));
-	memset(ch,0,sizeof(ch));
-	memset(key,0,sizeof(key));
+	memset(sz, 0, sizeof(sz));
+	memset(fa, 0, sizeof(fa));
+	memset(ch, 0, sizeof(ch));
+	memset(key, 0, sizeof(key));
 }
-int get_kth(int r,int k)
+int get_kth(int r, int k)
 {
 	push_down(r);
 	int tempszMax = sz[ch[r][0]] + cnt[r];
 	int tempszMin = sz[ch[r][0]] + 1;
-	if(k >= tempszMin && k <= tempszMax)
+	if (k >= tempszMin && k <= tempszMax)
 		return r;
-	else if( k < tempszMin)
-		return get_kth(ch[r][0],k);
+	else if (k < tempszMin)
+		return get_kth(ch[r][0], k);
 	else
-		return get_kth(ch[r][1],k-tempszMax);
+		return get_kth(ch[r][1], k - tempszMax);
 }
-int FIND(int r,int k)
+int FIND(int r, int k)
 {
-	if(!r)
+	if (!r)
 		return -1;
-	if(key[r] == k)
+	if (key[r] == k)
 	{
-		splay(r,0);
+		splay(r, 0);
 		return r;
 	}
-	FIND(ch[r][k > key[r]],k);
+	return FIND(ch[r][k > key[r]], k);
 }
 int get_Min(int r)
 {
 	push_down(r);
-	while(ch[r][0])
+	while (ch[r][0])
 	{
 		r = ch[r][0];
 		push_down(r);
@@ -159,7 +159,7 @@ int get_Min(int r)
 int get_Max(int r)
 {
 	push_down(r);
-	while(ch[r][1])
+	while (ch[r][1])
 	{
 		r = ch[r][1];
 		push_down(r);
@@ -169,34 +169,34 @@ int get_Max(int r)
 //ch[ch[root][1]][0]
 void cut(int val)
 {
-	int r = FIND(root,val);
-	if(r== -1)
+	int r = FIND(root, val);
+	if (r == -1)
 		return;
-	splay(r,0);
-	if(cnt[r] > 1)
+	splay(r, 0);
+	if (cnt[r] > 1)
 	{
 		cnt[r]--;
 		return;
 	}
-	if(ch[r][0] && ch[r][1])
+	if (ch[r][0] && ch[r][1])
 	{
 		int old = root;
-		int lc = ch[r][0],rc = ch[r][1];
+		int lc = ch[r][0], rc = ch[r][1];
 		int tar = lc;
-		while(ch[tar][1])
+		while (ch[tar][1])
 			tar = ch[tar][1];
 		ch[tar][1] = rc;
 		fa[rc] = tar;
 		push_up(tar);
 		root = lc;
 		fa[lc] = 0;
-		splay(tar,0);
+		splay(tar, 0);
 		clean(old);
 	}
-	else if(ch[r][0] || ch[r][1])
+	else if (ch[r][0] || ch[r][1])
 	{
 		int old = root;
-		root = max(ch[r][0],ch[r][1]);
+		root = max(ch[r][0], ch[r][1]);
 		fa[root] = 0;
 		clean(old);
 	}
@@ -206,64 +206,68 @@ void cut(int val)
 		root = 0;
 	}
 }
-int get_kless(int r,int k)
+int get_kless(int r, int k)
 {
 	int sum = 0;
-	if(!r)
+	if (!r)
 		return 0;
-	if(key[r] > k)
-		return get_kless(ch[r][0],k);
-	else if(key[r] < k)	
+	if (key[r] > k)
+		return get_kless(ch[r][0], k);
+	else if (key[r] < k)
 	{
 		sum += cnt[r];
-		if(ch[r][0])
-			sum += get_kless(ch[r][0],k);
-		if(ch[r][1])
-			sum += get_kless(ch[r][1],k);
+		if (ch[r][0])
+			sum += sz[ch[r][0]];
+		if (ch[r][1])
+			sum += get_kless(ch[r][1], k);
 		return sum;
 	}
-	else if(key[r] == k)
+	else if (key[r] == k)
 	{
-		splay(r,0);
-		return 1;
+		sum = 1;
+		if (ch[r][0])
+			sum += sz[ch[r][0]];
+		splay(r, 0);
+		return sum;
 	}
+	return sum;
 }
 int main()
 {
 	int n;
-	while(scanf("%d",&n) == 1)
+	while (scanf("%d", &n) == 1)
 	{
 		init();
-		for(int i = 0;i<n;i++)
+		for (int i = 0; i < n; i++)
 		{
-			int op,x;
-			scanf("%d%d",&op,&x);
-			if(op == 1)
-				insert(root,x);
-			else if(op == 2)
+			int op, x;
+			scanf("%d%d", &op, &x);
+			if (op == 1)
+				insert(root, x);
+			else if (op == 2)
 				cut(x);
-			else if(op == 3)
-				printf("%d\n",get_kless(root,x));
-			else if(op == 4)
-				printf("%d\n",key[get_kth(root,x)]);
-			else if(op == 5)
+			else if (op == 3)
+				printf("%d\n", get_kless(root, x));
+			else if (op == 4)
+				printf("%d\n", key[get_kth(root, x)]);
+			else if (op == 5)
 			{
-				insert(root,x);
-				int Index = FIND(root,x);
-				if(Index == -1)
+				insert(root, x);
+				int Index = FIND(root, x);
+				if (Index == -1)
 					continue;
-				splay(Index,0);
-				printf("%d\n",key[get_Max(ch[root][0])]);
+				splay(Index, 0);
+				printf("%d\n", key[get_Max(ch[root][0])]);
 				cut(x);
 			}
-			else 
+			else
 			{
-				insert(root,x);
-				int Index = FIND(root,x);
-				if(Index == -1)
+				insert(root, x);
+				int Index = FIND(root, x);
+				if (Index == -1)
 					continue;
-				splay(Index,0);
-				printf("%d\n",key[get_Min(ch[root][1])]);
+				splay(Index, 0);
+				printf("%d\n", key[get_Min(ch[root][1])]);
 				cut(x);
 			}
 			//cout << " * ";
