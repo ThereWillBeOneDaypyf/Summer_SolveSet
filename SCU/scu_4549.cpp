9663acc0b066e88cfdd045b44d78af922fb68620
ThereWillBeOneDaypyf <527908203@qq.com>
2017-09-13 21:04:07 +0800

    2017年 09月 13日 星期三 21:04:07 CST

---
+++ SCU/scu_4549.cpp
@@ -0,0 +1,204 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//thanks to pyf ...
+//thanks to qhl ...
+
+const int N = 1e5+7;
+
+struct Edge
+{
+	int u,v,next;
+	long long d;
+}edge[N*2];
+
+struct Tree
+{
+	int l,r;
+	long long sum;
+	long long vis;
+}t[N*4];
+
+int tot = 0;
+int pos = 0;
+
+int sz[N],fa[N],top[N],bigson[N],deep[N],w[N];
+int head[N];
+
+void init()
+{
+	tot = 0;
+	pos = 0;
+	memset(head,-1,sizeof(head));
+}
+void add_edge(int u,int v,int d)
+{
+	edge[tot].u = u;
+	edge[tot].v = v;
+	edge[tot].d = d;
+	edge[tot].next = head[u];
+	head[u] = tot ++;
+}
+
+void dfs(int u,int Fa,long long d)
+{
+	fa[u] = Fa;
+	sz[u] = 1;
+	deep[u] = d;
+	bigson[u] = -1;
+	for(int i = head[u];i!=-1;i=edge[i].next)
+	{
+		int v = edge[i].v;
+		if(v == Fa)
+			continue;
+		dfs(v,u,d+1);
+		sz[u] += sz[v];
+		if(bigson[u] == -1 || sz[v] > sz[bigson[u]])
+			bigson[u] = v;
+	}
+}
+void link(int u,int tp)
+{
+	top[u] = tp;
+	w[u] = ++pos;
+	if(bigson[u] != -1)
+		link(bigson[u],tp);
+	for(int i = head[u];i!=-1;i=edge[i].next)
+	{
+		int v = edge[i].v;
+		if(v == bigson[u] || v == fa[u])
+			continue;
+		link(v,v);
+	}
+}
+void push_up(int step)
+{
+	t[step].sum = t[step*2].sum + t[step*2+1].sum;
+}
+void push_down(int step)
+{
+	if(!t[step].vis)
+		return;
+	t[step*2].vis += t[step].vis;
+	t[step*2+1].vis += t[step].vis;
+	t[step*2].sum += 1LL * (t[step*2].r - t[step*2].l +1) * t[step].vis;
+	t[step*2+1].sum += 1LL * (t[step*2+1].r - t[step*2+1].l + 1) * t[step].vis;
+	t[step].vis = 0;
+}
+void build(int l,int r,int step)
+{
+	t[step].l = l,t[step].r = r,t[step].sum = 0,t[step].vis = 0;
+	if(l == r)
+		return;
+	int mid = (l + r) / 2;
+	build(l,mid,step*2);
+	build(mid+1,r,step*2+1);
+	push_up(step);
+}
+void update(int l,int r,long long val,int step)
+{
+	if(t[step].l == l && t[step].r == r)
+	{
+		t[step].sum += 1LL * (t[step].r - t[step].l + 1) * val;
+		t[step].vis += val;	
+		return;
+	}
+	int mid = (t[step].l + t[step].r) / 2;
+	push_down(step);
+	if(r <= mid)
+		update(l,r,val,step*2);
+	else if(l > mid)
+		update(l,r,val,step*2+1);
+	else
+		update(l,mid,val,step*2),update(mid+1,r,val,step*2+1);
+	push_up(step);
+}	
+long long query(int l,int r,int step)
+{
+	if(t[step].l == l && t[step].r == r)
+		return t[step].sum;
+	int mid = (t[step].l + t[step].r) / 2;
+	push_down(step);
+	if(r <= mid)
+		return query(l,r,step*2);
+	else if(l >  mid)
+		return query(l,r,step*2+1);
+	else return query(l,mid,step*2) + query(mid+1,r,step*2+1);
+}
+void change(int x,int y,long long val)
+{
+	while(top[x] != top[y])
+	{
+		if(deep[top[x]] < deep[top[y]])
+			swap(x,y);
+		update(w[top[x]],w[x],val,1);
+		x = fa[top[x]];
+	}
+	if(x == y)
+		return;
+	if(deep[x] > deep[y])
+		swap(x,y);
+	update(w[bigson[x]],w[y],val,1);
+}
+long long get_sum(int x,int y)
+{
+	long long sum = 0;
+	while(top[x] != top[y])
+	{
+		if(deep[top[x]] < deep[top[y]])
+			swap(x,y);
+		sum += query(w[top[x]],w[x],1);
+		x = fa[top[x]];	
+	}
+	/*edge to point*/
+	if(x == y)
+		return sum;
+	if(deep[x] > deep[y])
+		swap(x,y);
+	sum += query(w[bigson[x]],w[y],1);
+	return sum;	
+} 
+int main()
+{
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+		init();
+		int n,m;
+		scanf("%d%d",&n,&m);
+		for(int i = 1;i < n;i ++)
+		{
+			int u,v,d;
+			scanf("%d%d%lld",&u,&v,&d);
+			add_edge(u,v,d),add_edge(v,u,d);
+		}
+		dfs(1,1,0);
+		link(1,1);
+		build(1,n,1);
+		char op[10];
+		int x,y,z;
+		for(int i = 0;i<tot;i++)
+		{
+			int u = edge[i].u , v = edge[i].v ;
+			long long d = edge[i].d;
+			if(u < v)
+				change(u,v,d);
+		}
+		for(int i = 0;i<m;i++)
+		{
+			scanf("%s",op);
+			if(op[0] == 'Q')
+			{
+				scanf("%d%d",&x,&y);
+				printf("%lld\n",get_sum(x,y));
+			}
+			else
+			{
+				scanf("%d%d%d",&x,&y,&z);
+				change(x,y,z);
+			}
+		}
+	}
+	return 0;
+}
--- hdu/hdu_6200.cpp
+++ hdu/hdu_6200.cpp
@@ -3,331 +3,218 @@
 #include<algorithm>
 #include<cstring>
 #include<vector>
-#pragma comment(linker, "/STACK:1024000000,1024000000");
 using namespace std;
 
 //thanks to pyf ...
 //thanks to qhl ...
-const int MAXBUF = 10000;
-char buf[MAXBUF], *ps = buf, *pe = buf + 1;
-inline void rnext()
-{
-	if (++ps == pe)
-		pe = (ps = buf) + fread(buf, sizeof(char), sizeof(buf) / sizeof(char), stdin);
-}
 
-template <class T>
-inline bool in(T &ans)
-{
-	ans = 0;
-	T f = 1;
-	if (ps == pe) return false; //EOF
-	do
-	{
-		rnext();
-		if ('-' == *ps) f = -1;
-	}
-	while (!isdigit(*ps) && ps != pe);
-	if (ps == pe) return false; //EOF
-	do
-	{
-		ans = (ans << 1) + (ans << 3) + *ps - 48;
-		rnext();
-	}
-	while (isdigit(*ps) && ps != pe);
-	ans *= f;
-	return true;
-}
-const int  MAXOUT = 10000;
-char bufout[MAXOUT], outtmp[50], *pout = bufout, *pend = bufout + MAXOUT;
-inline void write()
-{
-	fwrite(bufout, sizeof(char), pout - bufout, stdout);
-	pout = bufout;
-}
-inline void out_char(char c) { *(pout++) = c; if (pout == pend) write();}
-inline void out_str(char *s)
-{
-	while (*s)
-	{
-		*(pout++) = *(s++);
-		if (pout == pend) write();
-	}
-}
-template <class T>
-inline void out_int(T x)
-{
-	if (!x)
-	{
-		out_char('0');
-		return;
-	}
-	if (x < 0) x = -x, out_char('-');
-	int len = 0;
-	while (x)
-	{
-		outtmp[len++] = x % 10 + 48;
-		x /= 10;
-	}
-	outtmp[len] = 0;
-	for (int i = 0, j = len - 1; i < j; i++, j--) swap(outtmp[i], outtmp[j]);
-	out_str(outtmp);
-}
 const int N = 1e5 + 1;
-vector<int>G[N], g[N];
-vector<pair<int, int> > edges;
-vector<pair<int, int> > e;
-int low[N], dfn[N];
-int fa[N];
-int Index = 0, pos = 0;
+
+vector<pair<int,int> > edges;
+vector<int> G[N],g[N];
+bool is_cut[N*2];
+int dfn[N],low[N],L[N],R[N];
+int Index = 0,id = 0;
+int fa[N],f[N][21],dep[N];
 int n;
 void init()
 {
-	Index = pos = 0;
-	for (int i = 0; i < N; i++)
-		G[i].clear(), g[i].clear(), fa[i] = i;
-	memset(dfn, 0, sizeof(dfn));
-	memset(low, 0, sizeof(low));
-	e.clear();
+	Index = 0, id = 0;
+	memset(dfn,0,sizeof(dfn));
+	memset(is_cut,0,sizeof(is_cut));
+	for(int i = 0;i<N;i++)
+		G[i].clear(),g[i].clear(),fa[i] = i;
 	edges.clear();
 }
-void add_edge(int u, int v)
+void add_edge(int u,int v)
 {
-	edges.push_back(make_pair(u, v));
-	int pos = edges.size() - 1;
+	edges.push_back(make_pair(u,v));
 	G[u].push_back(edges.size() - 1);
+	edges.push_back(make_pair(v,u));
 	G[v].push_back(edges.size() - 1);
 }
-void tarjan(int u, int Fa)
+void tarjan(int u,int Fa)
 {
 	dfn[u] = low[u] = ++ Index;
-	for (int i = 0; i != G[u].size(); i ++)
+	for(int i = 0;i != G[u].size();i ++)
 	{
-		int id = G[u][i];
-		int v;
-		if (u == edges[id].first)
-			v = edges[id].second;
-		else
-			v = edges[id].first;
-		if (v == Fa)
+		int v = edges[G[u][i]].second;
+		if(v == Fa)
 			continue;
-		if (!dfn[v])
+		if(!dfn[v])
 		{
-			tarjan(v, u);
-			low[u] = min(low[u], low[v]);
-			if (low[v] > dfn[u])
-				e.push_back(make_pair(min(u, v), max(u, v)));
-		}
-		else low[u] = min(low[u], dfn[v]);
+			tarjan(v,u);
+			low[u] = min(low[v],low[u]);
+			if(low[v] > dfn[u])
+				is_cut[G[u][i]] = is_cut[G[u][i] ^ 1] = true;
+		}	
+		else low[u] = min(low[u],dfn[v]);
 	}
 }
 int find(int x)
 {
-	if (fa[x] != x)
+	if(x != fa[x])
 		fa[x] = find(fa[x]);
 	return fa[x];
 }
-void merge(int a, int b)
+void merge(int u,int v)
 {
-	a = find(a), b = find(b);
-	if (a != b)
-		fa[a] = b;
+	u = find(u), v = find(v);
+	if(u == v)
+		return;
+	if(dep[u] > dep[v])
+		swap(u,v);
+	fa[v] = u;
 }
-void Create_g()
+void Create_Graph()
 {
-	for (int i = 1; i <= n; i++)
-		if (!dfn[i])
-			tarjan(i, i);
-	for (int i = 0; i < N; i++)
-		G[i].clear();
-	sort(e.begin(), e.end());
-	for (int i = 0; i != edges.size(); i ++)
+	for(int i = 1;i<=n;i++)
+		tarjan(i,i);	
+	for(int i = 0;i<edges.size();i++)
 	{
 		int u = edges[i].first, v = edges[i].second;
-		vector<pair<int, int> > :: iterator pos = lower_bound(e.begin(), e.end(), make_pair(min(u, v), max(u, v)));
-		if (pos == e.end() || *pos != make_pair(min(u, v), max(u, v)))
-			merge(u, v);
+		if(!is_cut[i])
+			merge(u,v);
 	}
-	edges.clear();
-	for (int i  = 0; i != e.size(); i++)
+	for(int i = 0;i<edges.size();i++)
 	{
-		int u = find(e[i].first), v = find(e[i].second);
-		g[u].push_back(v), g[v].push_back(u);
+		int u = edges[i].first, v = edges[i].second;
+		if(is_cut[i])
+			g[find(u)].push_back(find(v));
+	}
+	for(int i = 1;i<=n;i++)
+	{
+		sort(g[i].begin(),g[i].end());
+		g[i].erase(unique(g[i].begin(),g[i].end()),g[i].end());
 	}
 }
-// heavy light
-int sz[N], f[N], top[N], bigson[N], dep[N], w[N];
-void dfs(int u, int Fa, int d)
+void dfs(int u,int Fa,int d)
 {
-	dep[u] = d, f[u] = Fa, sz[u] = 1, bigson[u] = -1;
-	for (int i = 0; i != g[u].size(); i ++)
+	dep[u] = d,f[u][0] = Fa;
+	for(int i = 1;i<=20;i++)
+		f[u][i] = f[f[u][i-1]][i-1];
+	L[u] = ++ id;
+	for(int i = 0;i != g[u].size();i ++)
 	{
 		int v = g[u][i];
-		if (v == Fa)
+		if(v == Fa)
 			continue;
-		dfs(v, u, d + 1);
-		sz[u] += sz[v];
-		if (bigson[u] == -1 || sz[bigson[u]] < sz[v])
-			bigson[u] = v;
+		dfs(v,u,d+1);
 	}
+	R[u] = id;
 }
-void link(int u, int tp)
+int lca(int u,int v)
 {
-	top[u] = tp;
-	w[u] = ++ pos;
-	if (bigson[u] != -1)
-		link(bigson[u], tp);
-	for (int i = 0; i != g[u].size(); i++)
-	{
-		int v = g[u][i];
-		if (v == f[u] || v == bigson[u])
-			continue;
-		link(v, v);
-	}
+	if(dep[u] < dep[v])
+		swap(u,v);
+	for(int i = 20;i>=0;i--)
+		if(dep[f[u][i]] >= dep[v])
+			u = f[u][i];
+	if(u == v)
+		return u;
+	for(int i = 20;i>=0;i--)
+		if(f[u][i] != f[v][i])
+			u = f[u][i], v = f[v][i];
+	return f[u][0];
 }
-//segment tree
 struct Tree
 {
-	int vis, sum;
-} t[N * 4];
-void push_up(int step)
+	int vis,sum;
+}t[N*4];
+void push_down(int l,int r,int step)
 {
-	t[step].sum = t[step * 2].sum + t[step * 2 + 1].sum;
-}
-void push_down(int l, int r, int step)
-{
-	if (t[step].vis == -1)
-		return;
 	int mid = (l + r) / 2;
-	t[step * 2].vis = t[step * 2 + 1].vis = t[step].vis;
-	t[step * 2].sum = (mid - l + 1) * t[step].vis;
-	t[step * 2 + 1].sum = (r - (mid + 1) + 1) * t[step].vis;
-	t[step].vis = -1;
+	if(!t[step].vis)
+		return;
+	t[step * 2].vis += t[step].vis;
+	t[step * 2 + 1].vis += t[step].vis;
+	t[step * 2].sum += t[step].vis * (mid - l + 1);
+	t[step * 2 + 1].sum += t[step].vis * (r - (mid + 1) + 1);
+	t[step].vis = 0;
 }
-void build(int l, int r, int step)
+void build()
 {
-	memset(t, 0, sizeof(t));
-	for (int i = 0; i < N * 4; i++)
-		t[i].vis = -1;
+	memset(t,0,sizeof(t));
 }
-void update(int ql, int qr, int l, int r, int val, int step)
+void update(int l,int r,int ql,int qr,int val,int step)
 {
-	if (ql == l && qr == r)
+	if(l == ql && r == qr)
 	{
-		t[step].vis = val;
-		t[step].sum = (r - l + 1) * val;
+		t[step].vis += val;
+		t[step].sum += (r - l + 1) * val;
 		return;
 	}
 	int mid = (l + r) / 2;
-	push_down(l, r, step);
-	if (qr <= mid)
-		update(ql, qr, l, mid, val, step * 2);
-	else if (ql > mid)
-		update(ql, qr, mid + 1, r, val, step * 2 + 1);
-	else update(ql, mid, l, mid, val, step * 2), update(mid + 1, qr, mid + 1, r, val, step * 2 + 1);
-	push_up(step);
+	push_down(l,r,step);
+	if(qr <= mid)
+		update(l,mid,ql,qr,val,step*2);
+	else if(ql > mid)
+		update(mid + 1,r, ql,qr,val,step*2+1);
+	else update(l,mid,ql,mid,val,step * 2),update(mid+1,r,mid+1,qr,val,step*2+1);
 }
-int query(int ql, int qr, int l, int r, int step)
+int query(int x,int l,int r,int step)
 {
-	if (l == ql && r == qr)
+	if(l == r)
 		return t[step].sum;
 	int mid = (l + r) / 2;
-	push_down(l, r, step);
-	if (qr <= mid)
-		return query(ql, qr, l, mid, step * 2);
-	else if (ql > mid)
-		return query(ql, qr, mid + 1, r, step * 2 + 1);
-	else return query(ql, mid, l, mid, step * 2) + query(mid + 1, qr, mid + 1, r, step * 2 + 1);
+	push_down(l,r,step);
+	if(x <= mid)
+		return query(x,l,mid,step*2);
+	else return query(x,mid+1,r,step*2+1);
 }
-void change(int u, int v, int val)
+void link(int u,int v)
 {
-	while (top[u] != top[v])
+	u = find(u), v = find(v);
+	int anc = find(lca(u,v));
+	while(find(u) != find(anc))
 	{
-		if (dep[top[u]] < dep[top[v]])
-			swap(u, v);
-		update(w[top[u]], w[u], 1, n, val, 1);
-		u = f[top[u]];
+		update(1,n,L[find(u)],R[find(u)],-1,1);
+		merge(find(u),f[find(u)][0]);
 	}
-	if (u == v)
-		return;
-	if (dep[u] > dep[v])
-		swap(u, v);
-	update(w[bigson[u]], w[v], 1, n, val, 1);
+	while(find(v) != find(anc))
+	{
+		update(1,n,L[find(v)],R[find(v)],-1,1);
+		merge(find(v),f[find(v)][0]);
+	}
 }
-int get_sum(int u, int v)
+int Get_Ans(int u,int v)
 {
-	int sum = 0;
-	while (top[u] != top[v])
-	{
-		if (dep[top[u]] < dep[top[v]])
-			swap(u, v);
-		sum += query(w[top[u]], w[u], 1, n, 1);
-		u = f[top[u]];
-	}
-	if (u == v)
-		return sum;
-	if (dep[u] > dep[v])
-		swap(u, v);
-	sum += query(w[bigson[u]], w[v], 1, n , 1);
-	return sum;
+	u = find(u), v = find(v);
+	int anc = find(lca(u,v));
+	return query(L[u],1,n,1) + query(L[v],1,n,1) - 2 * query(L[anc],1,n,1);
 }
 int main()
 {
 	int T;
-	freopen("1.in", "r", stdin);
-	in(T);
+	scanf("%d", &T);
 	int ka = 0;
-	while (T--)
+	while(T--)
 	{
 		int m;
-		in(n), in(m);
 		init();
-		for (int i = 0; i < m; i++)
+		scanf("%d%d",&n,&m);	
+		for(int i = 0;i<m;i++)
 		{
-			int u, v;
-			in(u), in(v);
-			add_edge(u, v);
+			int u,v;
+			scanf("%d%d",&u,&v);
+			add_edge(u,v);
 		}
-		Create_g();
-		dfs(find(1), find(1), 0);
-		link(find(1), find(1));
-		build(1, n, 1);
-		for (int i = 0; i != e.size(); i ++)
-		{
-			int u = e[i].first, v = e[i].second;
-			u = find(u), v = find(v);
-			//		cout << "*" <<  u << " " << v << endl;
-			if (dep[u] < dep[v])
-				swap(u, v);
-			update(w[u], w[u], 1, n, 1, 1);
-			//change(u, u, 1);
-		}
-		// cout << "*";
-		// debug(1);
-		// cout << endl;
+		Create_Graph();
+		build();
+		dfs(find(1),find(1),0);
+		for(int i = 1;i<=n;i++)
+			if(find(i) == i)
+				update(1,n,L[i],L[i],dep[i],1);
 		int q;
-		in(q);
-		out_str("Case #"); out_int(++ka); out_str(":\n");
-		// for (int i = 1; i <= n; i++)
-		// 	cout << i << " " << find(i) << endl;
-		for (int i = 0; i < q; i++)
+		scanf("%d",&q);
+		printf("Case #%d:\n",++ka);
+		for(int i = 0;i<q;i++)
 		{
-			int op, u, v;
-			in(op), in(u), in(v);
-			if (op == 1)
-			{
-				change(find(u), find(v), 0);
-				// if (dep[find(u)] < dep[find(v)])
-				// 	swap(u, v);
-				// change(find(u), find(v), 0);
-				// change(find(v), find(v), 1);
-			}
-			else
-				out_int(get_sum(find(u), find(v))), out_char('\n');
-			// cout << "*" ;
-			// debug(1);
-			// cout << endl;
+			int op,x,y;
+			scanf("%d%d%d",&op,&x,&y);
+			if(op == 1)
+				link(x,y);
+			else printf("%d\n",Get_Ans(x,y));
 		}
 	}
-	write();
 }
